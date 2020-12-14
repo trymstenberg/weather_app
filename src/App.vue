b2f828b579b415c76a31db0742ead46bc3456f66
Trym <s341827@oslomet.no>
2020-12-14 12:38:15 +0100

    legger til styling

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,7 @@ export default {
 }
 
 body{
-  font-family: 'monserat', sans-serif;
+  font-family: 'montserrat', sans-serif;
 }
 
 #app {
@@ -39,8 +39,38 @@ body{
 }
 
 main{
-  min-width: 100vh;
+  min-height: 100vh;
   padding: 25px;
+
+  background-image: linear-gradient(to bottom, rgba(0,0,0,0.25), rgba(0,0,0,0.75));
+}
+
+.search-box{
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar{
+  display: block;
+  width: 100%;
+  padding: 15px;
+
+  color: #313131;
+  font-size: 20px;
+  
+
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar{
+  background-color: rgba(255, 255, 255, 0.75);
 }
 </style>
     
